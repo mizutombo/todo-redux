bfbc3ba11f08be49f38383b92a80258ea5495fe5
mizutombo <thomas.shultz52@gmail.com>
2017-03-16 10:53:32 -0700

    adds action creators to actions / index.js

---
+++ src/actions/index.js
@@ -0,0 +1,40 @@
+let nextTodoId = 0;
+
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  };
+};
+
+export const deleteTodo = (id) => {
+  return {
+    type: 'DELETE_TODO',
+    id
+  };
+};
+
+export const editTodo = (id) => {
+  return {
+    type: 'EDIT_TODO',
+    id
+  };
+};
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  };
+};
+
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  };
+};
+
+
+
