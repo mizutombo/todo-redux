4c1232ddbd442d4fd5373104f7a90e0c94fdcc7b
mizutombo <thomas.shultz52@gmail.com>
2017-03-20 09:31:36 -0700

    adds AddTodo.js code per redux docs guidelines

---
+++ src/presentation-components/AddTodo.js
@@ -0,0 +1,34 @@
+import React, { Component, PropTypes } from 'react';
+import { addTodo } from '../actions/actionTypes';
+import { connect } from 'react-redux';
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAdd(newTodoItem) {
+      dispatch(addTodo(newTodoItem));
+    }
+  };
+};
+
+class AddTodo extends Component {
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          this.props.onAdd(
+            this.ref.text.value
+          );
+        }}>
+          <input ref='text' placeholder='new todo item' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(mapDispatchToProps)(addTodo);
+
+AddTodo.propTypes = {
+  onAdd: PropTypes.func.isRequired,
+};
