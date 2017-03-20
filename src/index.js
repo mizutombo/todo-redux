80f3fe82bffbbcf715ac3f4261d30ee486c4dbce
mizutombo <thomas.shultz52@gmail.com>
2017-03-20 11:17:09 -0700

    corrects App import path in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers/index';
-import App from './components/App';
+import App from './presentation-components/App';
 
 let store = createStore(todoApp);
 
