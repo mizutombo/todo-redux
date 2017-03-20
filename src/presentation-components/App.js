d93fe7113b422f2a7d5eda206b0517a26ccaf9f1
mizutombo <thomas.shultz52@gmail.com>
2017-03-20 10:23:20 -0700

    adds App.js

---
+++ src/presentation-components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddTodo from '../container-components/AddTodo';
+import TodoList from './TodoList';
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <TodoList />
+  </div>
+)
+
+export default App;
