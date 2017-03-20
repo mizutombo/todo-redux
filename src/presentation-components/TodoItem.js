46f94a63947840609960d8b2ee07133355ae899c
mizutombo <thomas.shultz52@gmail.com>
2017-03-20 06:33:33 -0700

    adds TodoItem.js

---
+++ src/presentation-components/TodoItem.js
@@ -0,0 +1,42 @@
+import React, { PropTypes } from 'react';
+import { deleteTodo, editTodo, completeTodo } from '../actions/actionTypes';
+import { connect } from 'react-redux';
+
+const mapDispatchToProps = (dispatch) => { 
+  return {
+    onDelete(todoId) {
+      dispatch(deleteTodo(todoId));
+    },
+    onEdit(todo) {
+      dispatch(editTodo(todo));
+    },
+    onComplete(todo) {
+      dispatch(completeTodo(todo));
+    }
+  };
+};
+
+function TodoItem(props) { 
+  return (
+    <li>
+      <input type='checkbox' checked={props.todo.complete} onChange={() => {
+        props.onComplete(props.todo);
+      }} />
+      <label> {props.todo.text} </label>
+      <button onClick={(event) => {
+        event.preventDefault();
+        props.onDelete(
+          props.todo.id
+        );
+      }}> done </button> 
+    </li>
+  );
+};
+
+export default connect(mapDispatchToProps)(TodoItem);
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onComplete: PropTypes.func.isRequired,
+};
