66af87b1945705f673919df98edec298d180e000
mizutombo <thomas.shultz52@gmail.com>
2017-03-20 06:33:49 -0700

    adds TodoList.js

---
+++ src/presentation-components/TodoList.js
@@ -0,0 +1,24 @@
+import React, {PropTypes } from 'react';
+import TodoItem from './TodoItem';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map(todo =>
+        <TodoItem key={todo.id} todo={todo} />)}
+    </ul>
+  );
+};
+
+export default connect(mapStateToProps)(TodoList);
+
+TodoList.propTypes = {
+  todos: PropTypes.object.isRequired, 
+};
